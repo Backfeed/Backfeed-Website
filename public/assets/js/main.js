7e427c7ac9c481e9ebdb1d57246e4155734d1c3c
Ore Landau <Ore4444@gmail.com>
2016-01-03 22:01:36 +0200

    eframe implementation pretty much

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -5,30 +5,7 @@ $(document).ready(function() {
 
     mediaCheck({
         media: '(min-width: 768px)',
-        entry: function() {
-            var currentIndex = $('.fp-section').index($('.active'));
-
-            $.fn.fullpage.destroy('all');
-
-            var protocolSection = $('#infrastructure-for-decentralized-cooperation');
-            var protocolSectionRow = $('<div class="row"></div>').appendTo(protocolSection);
-
-            var protocolSectionNextCol = $('<div class="col-sm-4"></div>').appendTo(protocolSectionRow);
-            protocolSection.next().find('.protocol-feature').appendTo(protocolSectionNextCol);
-            protocolSection.next().remove();
-
-            protocolSectionNextCol = $('<div class="col-sm-4"></div>').appendTo(protocolSectionRow);
-            protocolSection.next().find('.protocol-feature').appendTo(protocolSectionNextCol);
-            protocolSection.next().remove();
-
-            protocolSectionNextCol = $('<div class="col-sm-4"></div>').appendTo(protocolSectionRow);
-            protocolSection.next().find('.protocol-feature').appendTo(protocolSectionNextCol);
-            protocolSection.next().remove();
-
-            initFullPage(fullPageContainer);
-
-            if (currentIndex !== -1 ) $.fn.fullpage.moveTo(currentIndex);
-        },
+        entry: mobilizeProtocolSection,
         exit: function() {
             console.log('leaving 420');
         },
@@ -37,10 +14,60 @@ $(document).ready(function() {
         }
     });
 
+    function mobilizeProtocolSection() {
+        $.fn.fullpage.destroy('all');
+
+        var protocolSection = $('#infrastructure-for-decentralized-cooperation');
+        var protocolSectionRow = $('<div class="row"></div>').appendTo(protocolSection);
+
+        var protocolSectionNextCol = $('<div class="col-sm-4"></div>').appendTo(protocolSectionRow);
+        protocolSection.next().find('.protocol-feature').appendTo(protocolSectionNextCol);
+        protocolSection.next().remove();
+
+        protocolSectionNextCol = $('<div class="col-sm-4"></div>').appendTo(protocolSectionRow);
+        protocolSection.next().find('.protocol-feature').appendTo(protocolSectionNextCol);
+        protocolSection.next().remove();
+
+        protocolSectionNextCol = $('<div class="col-sm-4"></div>').appendTo(protocolSectionRow);
+        protocolSection.next().find('.protocol-feature').appendTo(protocolSectionNextCol);
+        protocolSection.next().remove();
+
+        initFullPage(fullPageContainer);
+    }
+
     function initFullPage() {
         fullPageContainer.fullpage({
             "sectionSelector": "section",
-            "navigation": true
+            "navigation": true,
+            onLeave: function(index, nextIndex, direction) {
+                var mainHeader = $('body>header');
+                if (direction == "up") mainHeader.slideDown();
+                if (direction == "down") mainHeader.slideUp();
+            }
         });
+    }
+
+    var triggerBttn = document.getElementById('trigger-overlay'),
+        overlay = document.querySelector('.overlay'),
+        closeBttn = overlay.querySelector('button.overlay-close');
+
+    if (triggerBttn) triggerBttn.addEventListener( 'click', toggleOverlay );
+    if (closeBttn) closeBttn.addEventListener( 'click', toggleOverlay );
+
+    function toggleOverlay() {
+        if (overlay) {
+            if (overlay.classList.contains('open')) {
+                overlay.classList.remove('open');
+                overlay.classList.add('close');
+                var onEndTransitionFn = function(ev) {
+                    if (ev.propertyName !== 'visibility') return;
+                    this.removeEventListener('transitionend', onEndTransitionFn);
+                    overlay.classList.remove('close');
+                };
+                overlay.addEventListener('transitionend', onEndTransitionFn);
+            } else if (!overlay.classList.contains('close')) {
+                overlay.classList.add('open');
+            }
+        }
     }
 });
