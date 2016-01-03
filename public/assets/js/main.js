f312a007a797b59e032905de317799efb19ff6c3
Ore Landau <Ore4444@gmail.com>
2016-01-03 11:51:59 +0200

    homepage wirframe implemented

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,11 +1,29 @@
 $(document).ready(function() {
+
     var fullPageContainer = $('#fullpage');
-    if (fullPageContainer) fullPageContainer.fullpage({"sectionSelector": "section"});
+    if (fullPageContainer) initFullPage(fullPageContainer);
 
     mediaCheck({
-        media: '(max-width: 420px)',
+        media: '(min-width: 768px)',
         entry: function() {
-            console.log('starting 420');
+            $.fn.fullpage.destroy('all');
+
+            var protocolSection = $('#infrastructure-for-decentralized-cooperation');
+            var protocolSectionRow = $('<div class="row"></div>').appendTo(protocolSection);
+
+            var protocolSectionNextCol = $('<div class="col-sm-4"></div>').appendTo(protocolSectionRow);
+            protocolSection.next().find('.protocol-feature').appendTo(protocolSectionNextCol);
+            protocolSection.next().remove();
+
+            protocolSectionNextCol = $('<div class="col-sm-4"></div>').appendTo(protocolSectionRow);
+            protocolSection.next().find('.protocol-feature').appendTo(protocolSectionNextCol);
+            protocolSection.next().remove();
+
+            protocolSectionNextCol = $('<div class="col-sm-4"></div>').appendTo(protocolSectionRow);
+            protocolSection.next().find('.protocol-feature').appendTo(protocolSectionNextCol);
+            protocolSection.next().remove();
+
+            initFullPage(fullPageContainer);
         },
         exit: function() {
             console.log('leaving 420');
@@ -14,4 +32,11 @@ $(document).ready(function() {
             console.log('changing state');
         }
     });
+
+    function initFullPage() {
+        fullPageContainer.fullpage({
+            "sectionSelector": "section",
+            "navigation": true
+        });
+    }
 });
