fbd6a7676e3a9251bd606985c06c63a4cea29117
Ore Landau <Ore4444@gmail.com>
2016-04-21 10:51:45 +0300

    added technical resources

--- _harp/gulpFile.js
+++ _harp/gulpFile.js
@@ -12,8 +12,8 @@ gulp.task('minify', function(){
 
     gulp.src('public/assets/js/main.js')
         .pipe(uglify())
-        .pipe(rename({ basename: 'main.min' }))
-        .pipe(gulp.dest('public/assets/js'))
+        .pipe(rename({basename: 'main.min'}))
+        .pipe(gulp.dest('public/assets/js'));
 
     return gulp.src(['public/lib/bootstrap.js', 'public/lib/jquery.js', 'public/lib/mediaCheck.js'])
         .pipe(uglify())
